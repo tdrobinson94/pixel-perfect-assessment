8d1d0e86f64dd2dc25009434b866702ed07c7b8e
tdrobinson94 <tdrobinson94@gmail.com>
2017-01-12 13:07:54 -0500

    finishing up dropdown menu functionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,14 @@
 import $ from 'jquery';
-
+// Enable hamburger dropdown menu on click
 $('.hamburger').on("click", function(){
     $('.links').toggleClass('show')
 })
+
+// If window changes from mobile to desktop view
+function windowChange (){
+    if ($(window).width() >= 649) {
+        $('.links').removeClass('show')
+    }
+}
+
+$(window).resize(windowChange)
